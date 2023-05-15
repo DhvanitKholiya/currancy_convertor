d7765698ad363be0f4328a58d6b2fceb370aff63
DhvanitKholiya <dhvanitkholiya28@gmail.com>
2023-05-15 11:01:15 +0530

    Complete

---
+++ README.md
@@ -0,0 +1,10 @@
+# Currency Convertor
+
+Convertor
+
+## ScreenShot
+
+<img src = "https://user-images.githubusercontent.com/122794880/218811334-1cd218c9-bb58-4dbf-879d-1e97cf4d960e.jpeg" height="500px"/> |
+<img src = "https://user-images.githubusercontent.com/122794880/218811393-676e0537-c6d4-4812-952f-1bbf6e476906.jpeg" height="500px"/>
+
+
---
+++ lib/global.dart
@@ -0,0 +1,11 @@
+class Global {
+  static dynamic dropDownVal1 = 'USD';
+  static dynamic dropDownVal2 = 'INR';
+  static var dropDown1 = ['USD', 'INR', 'CAD', 'AUD'];
+  static var dropDown2 = ['INR', 'USD', 'CAD', 'AUD'];
+  static dynamic convertedAmount = 0;
+  static dynamic amount = 0;
+  static double converterAmount1 = 0;
+  static double converterToAmount2 = 0;
+  static String sign = "";
+}
